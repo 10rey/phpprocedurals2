9f33bd6d1c851c4d302203dcc4bfba41ae291648
Jess Rey Ocon <10reyocon@gmail.com>
2018-08-27 07:40:10 +0800

    Dynamic dropdowns

--- User/myfavorites.php
+++ User/myfavorites.php
@@ -36,7 +36,7 @@
 			foreach ($check as $new_check) {
 				echo $new_check . "<br>";
 
-				// code if plannin to save in database the checkbox
+				// code if planning to save in database, the checkbox.
 				// mysqli_query($connection, "INSERT INTO tbl_name (field_name) VALUES('$new_check') ");
 
 			}
@@ -71,3 +71,57 @@
 	<input type="submit" name="btnSubmit" value="submit"> 
 
 </form>
+
+<hr>
+
+<script type="text/javascript" src="js/jQuery.js"></script>
+<script>
+	
+	var Category = {
+		"Car": ["Honda", "BMW", "Mustang"],
+		"Food": ["Burger", "Maling", "Hotdog"],
+		"Beer": ["Red Horse", "Colt 45", "San Mig Light Apple"],
+		"Beverages": ["Coke", "Sarsi", "Royal"],
+	}
+
+	function category(value){
+
+		if (value.length == 0){ 
+
+			document.getElementById("choice").innerHTML = "<option></option>"; 
+
+		}
+		else{
+
+			var category_options = "";
+
+			for(category_name in Category[value]){
+
+				category_options += "<option name='choice' value='" + Category[value][category_name] + "'>" + Category[value][category_name] + "</option>";
+
+				document.getElementById("choice").innerHTML = category_options;
+
+			}
+
+		}
+
+	}
+
+</script>
+
+<select name="category" id="category" onChange="category(this.value);">
+
+	<option name="category" value="">Select Category by Clicking Here</option>
+
+	<option name="category" value="Car">Car</option>
+	<option name="category" value="value">Food</option>
+	<option name="category" value="Beer">Beer</option>
+	<option name="category" value="Beverages">Beverages</option>
+
+</select>
+
+<select name="choice" id="choice">
+	
+	<option name="choice" value="">Select Category First</option>
+
+</select>
